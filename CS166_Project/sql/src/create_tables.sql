dcd9d8d54e189963c0082fe5952044d0904e45c6
Katherine Gallaher <kgall005@well.cs.ucr.edu>
2015-05-31 22:07:36 -0700

    created csv files

--- CS166_Project/sql/src/create_tables.sql
+++ CS166_Project/sql/src/create_tables.sql
@@ -6,35 +6,37 @@ DROP TABLE USR;
 
 
 CREATE TABLE USR(
-	userId varchar(10) UNIQUE NOT NULL, 
-	password varchar(10) NOT NULL,
+	userId varchar(255) UNIQUE NOT NULL, 
+	password varchar(20) NOT NULL,
 	email text NOT NULL,
 	name char(50),
 	dateOfBirth date,
 	Primary Key(userId));
 
 CREATE TABLE WORK_EXPR(
-	userId char(10) NOT NULL, 
+	userId varchar(255) NOT NULL, 
 	company char(50) NOT NULL, 
 	role char(50) NOT NULL,
 	location char(50),
 	startDate date,
 	endDate date,
-	PRIMARY KEY(userId,company,role,startDate));
+	PRIMARY KEY(userId,company,role,startDate)),
+	FOREIGN KEY(userId) REFERENCES USR;
 
 CREATE TABLE EDUCATIONAL_DETAILS(
-	userId char(10) NOT NULL, 
+	userId varchar(255) NOT NULL, 
 	instituitionName char(50) NOT NULL, 
 	major char(50) NOT NULL,
 	degree char(50) NOT NULL,
 	startdate date,
 	enddate date,
-	PRIMARY KEY(userId,major,degree));
+	PRIMARY KEY(userId,major,degree)),
+	FOREIGN KEY(userId) REFERENCES USR;
 
 CREATE TABLE MESSAGE(
 	msgId integer UNIQUE NOT NULL, 
-	senderId char(10) NOT NULL,
-	receiverId char(10) NOT NULL,
+	senderId varchar(255) NOT NULL,
+	receiverId varchar(255) NOT NULL,
 	contents char(500) NOT NULL,
 	sendTime timestamp,
 	deleteStatus integer,
